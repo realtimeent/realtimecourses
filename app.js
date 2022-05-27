6dd484a530a80c5b2f5d182371baa75df90e26da
realtimeent <106385244+realtimeent@users.noreply.github.com>
2022-05-27 05:12:21 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+document.getElementById('topics').addEventListener('click', Show);
+function Show() {
+    document.getElementById('s-list').classList.toggle('active')
+}
+
+document.getElementById('about').addEventListener('click', About)
+function About() {
+    document.getElementById('about-2').classList.toggle('active')
+}
+const names = ['jane', 'james','allen','tim']
+
